84b96aee1dabbe785400abfa15dcb0ba4318b988
zhouchenhua <noatnu@foxmail.com>
2024-01-12 11:36:55 +0800

    fix

--- algs/src/main/java/org/sort/BubbleSort.java
+++ algs/src/main/java/org/sort/BubbleSort.java
@@ -19,19 +19,4 @@ public class BubbleSort {
         }
         return arr;
     }
-
-    @Deprecated
-    public static int[] sortBubble(int[] arr) {
-        int temp;
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = 1; j > i; j++) {
-                if (arr[j - 1] > arr[j]) {
-                    temp = arr[j - 1];
-                    arr[j - 1] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
-        return arr;
-    }
 }
