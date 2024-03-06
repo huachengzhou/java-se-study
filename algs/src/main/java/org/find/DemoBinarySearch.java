c39d9b17355d63e3dd0f3114a46956470358efd7
zhouchenhua <huachengzhou@hotmail.com>
2024-03-06 20:49:35 +0800

    左边二分查找  和 右边二分查找

--- algs/src/main/java/org/find/DemoBinarySearch.java
+++ algs/src/main/java/org/find/DemoBinarySearch.java
@@ -4,11 +4,38 @@ public class DemoBinarySearch {
 
     /**
      * 右二分查找
+     *
      * @param arr
      * @param target
-     * @return
+     * @return 找到返回正整数没找到返回负整数
      */
-    public static int rightMostBinarySearch1(int[] arr, int target){
+    public static int rightMostBinarySearch(int[] arr, int target) {
+        int i = 0;
+        int j = arr.length - 1;
+        //i-j 范围内元素
+        while (i <= j) {
+            //移位运算 右移1位
+            int mid = (i + j) >>> 1;
+            //目标值在左边
+            if (target < arr[mid]) {
+                j = mid - 1;
+                //目标值在右边
+            } else if (arr[mid] <= target) {
+                i = mid + 1;
+            }
+        }
+        //没找到返回最近的索引并且强行取负
+        return target == arr[i - 1] ? i - 1 : -(i - 1);
+    }
+
+    /**
+     * 右二分查找
+     *
+     * @param arr
+     * @param target
+     * @return 找到返回正整数没找到返回负整数
+     */
+    public static int rightMostBinarySearch1(int[] arr, int target) {
         int i = 0;
         int j = arr.length - 1;
         int candidate = -1;
@@ -38,7 +65,7 @@ public class DemoBinarySearch {
      *
      * @param arr
      * @param target
-     * @return
+     * @return 找到返回正整数没找到返回负整数
      */
     public static int leftMostBinarySearch1(int[] arr, int target) {
         int i = 0;
@@ -66,10 +93,38 @@ public class DemoBinarySearch {
     }
 
     /**
-     * 标准二分基础查找
+     * 左二分查找
+     *
      * @param arr
      * @param target
-     * @return
+     * @return 找到返回正整数没找到返回负整数
+     */
+    public static int leftMostBinarySearch(int[] arr, int target) {
+        int i = 0;
+        int j = arr.length - 1;
+        //i-j 范围内元素
+        while (i <= j) {
+            //移位运算 右移1位
+            int mid = (i + j) >>> 1;
+            //目标值在左边
+            if (target <= arr[mid]) {
+                j = mid - 1;
+                //目标值在右边
+            } else if (arr[mid] < target) {
+                i = mid + 1;
+            }
+        }
+        //没找到返回最近的索引并且强行取负
+        return target == arr[i] ? i : -i;
+    }
+
+
+    /**
+     * 标准二分基础查找
+     *
+     * @param arr
+     * @param target
+     * @return 找到返回正整数没找到返回-1
      */
     public static int binarySearch(int[] arr, int target) {
         int i = 0, j = arr.length - 1;
@@ -97,7 +152,7 @@ public class DemoBinarySearch {
      *
      * @param arr    升序数组
      * @param target 目标值
-     * @return
+     * @return 找到返回正整数没找到返回-1
      */
     public static int binarySearch1(int[] arr, int target) {
         int i = 0;
--- algs/src/test/java/www/other/DemoBinarySearchTest.java
+++ algs/src/test/java/www/other/DemoBinarySearchTest.java
@@ -9,6 +9,27 @@ import org.junit.Test;
  * 二分查找demo
  */
 public class DemoBinarySearchTest {
+
+    @Test
+    public void test5() {
+        int[] intArray = new int[]{6, 13, 16, 22, 24, 32, 40, 53, 61, 74, 94, 95, 97, 97, 99};
+        int target = 6;
+        int index = DemoBinarySearch.rightMostBinarySearch(intArray, target);
+        String str = index >= 0 ? String.valueOf(intArray[index]) : "";
+        System.out.println(index + " " + str);
+        Assert.assertEquals("不相等", str, String.valueOf(target));
+    }
+
+    @Test
+    public void test4() {
+        int[] intArray = new int[]{6, 13, 16, 22, 24, 32, 40, 53, 61, 74, 94, 95, 97, 97, 99};
+        int target = 75;
+        int index = DemoBinarySearch.leftMostBinarySearch(intArray, target);
+        String str = index >= 0 ? String.valueOf(intArray[index]) : "";
+        System.out.println(index + " " + str);
+        Assert.assertEquals("不相等", str, String.valueOf(target));
+    }
+
     @Test
     public void test3() {
         int[] intArray = new int[]{6, 13, 16, 22, 24, 32, 40, 53, 61, 74, 94, 95, 97, 97, 99};
